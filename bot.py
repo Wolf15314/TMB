cbc8f84ac6fc95afbf1f0d1eb7009257aa3658b6
AS_12012018_2A <ancyfer.w@gmail.com>
2021-07-23 16:28:09 +0300

    Comit1

--- bot.py
+++ bot.py
@@ -1,22 +1,45 @@
-import config
-import logging
-import psutil
-import aiogram
+import telebot
+import configure
+#from telebot import types
 
-from aiogram import Bot, Dispatcher, executor, types
+client = telebot.TeleBot(configure.config['token'])
 
-#log level
-logging.basicConfig(level=logging.INFO)
+@client.message_handler(content_types = ['text'])
+def get_text(message):
+    if message.text.lower() == 'привет':
+        client.send_message(message.chat.id, 'Привет неизвестный юзер!')
 
-#bot init
+#@client.message_handler(commands = ['get_info', 'info'])
+#def get_user_info(message):
+#   markup_inline = types.InLineKeyboardMarkup()
+#    item_yes = types.InLineKeyboardButton(text = 'ДА', callback_data = 'yes')
+#    item_no = types.InLineKeyboardButton(text = 'НЕТ', callback_data = 'no')
+#
+#   markup_inline.add(item_yes, item_no)
+#  client.send_message(message.chat.id, 'Желаете узнать иформацию о вас?',
+#        reply_markup = markup_inline )
 
-bot = Bot(token=config.TOKEN)
-dp = Dispatcher(bot)
+#@client.calback_query_handler(func = lambda call: True)
+#def answer(call):
+ #   if call.data == 'yes':
+ #       markup_reply = types.ReplayKeyboardMarkup(resize_keyboard = True)
+ #       item.id = KeyboardButton('Мой ID')
+ #       item_username = types.KeyboardDutton('МОЙ НИК')
+#
+ #       markup_reply.add(item_id, item_username)
+  #      client.send_message(call.message.chat.id, 'Нажмите на одну из кнопок',
+   #         reply_markup = markup_reply
+    #                        )
+#    elif call.data == 'no':
+ #       pass
 
-#echo
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer("memory used:" )
 
-#run long-polling
-if __name__ == "__main__":
+#@client.message_handler(content_types = ['text'])
+#def get_text(message):
+ #   if message.text == 'МОЙ ID':
+ #       client.send_message(message.chat.id, f'Your ID: {message.from_user.id}')
+ #   elif message.text == 'МОЙ НИК':
+  #      client.send_message(message.chat.id, f'Your ID: {message.from_user.first_name} {message.from_user.last_name}')
+#
+#
+client.polling(none_stop = True, interval = 0)
